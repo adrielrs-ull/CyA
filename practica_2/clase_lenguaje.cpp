6cf9c9a30c549fec60e5a2ee9dd0dce504a55938
usuario <alu0101640136@ull.edu.es>
2024-09-21 15:58:34 +0000

    Práctica acabada sin corrección de estética

--- practica_2/clase_lenguaje.cpp
+++ practica_2/clase_lenguaje.cpp
@@ -1,3 +1,17 @@
+// Universidad de La Laguna
+// Escuela Superior de Ingenier´ıa y Tecnolog´ıa
+// Grado en Ingenier´ıa Inform´atica
+// Asignatura: Computabilidad y Algoritmia
+// Curso: 2º
+// Pr´actica 2: Cadenas y lenguajes
+// Autor: Adriel Reyes Suárez
+// Correo: alu0101640136@ull.edu.es
+// Fecha: 17/09/2024
+// Clase lenguaje con atributos cadena
+// Referencias:
+// Enlaces de inter´es
+// Historial de revisiones
+// 17/09/2024 - Creaci´on (primera versi´on) del c´odigo
 #include <iostream>
 #include <set>
 #include "clase_cadena.h"
