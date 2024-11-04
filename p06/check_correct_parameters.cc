deacc8a929bf5f4226df0e1f6ef7db35f9e63a5b
adrielrs-ull <alu0101640136@ull.edu.es>
2024-11-04 12:05:06 +0000

    Modi check p06

--- p06/check_correct_parameters.cc
+++ p06/check_correct_parameters.cc
@@ -30,37 +30,36 @@
  * @return false 
  */
 bool CheckCorrectParameters(int argc, char* argv[], const int kLimit) {
-  std::string help = argv[1];
-  if (argc == kLimit) {
-    
-    return true;
-
-  } else if (argc == 2 && help == "--help") {
-    std::cout << "Uso: p06_automata_simulator [opciones]\n";
-    std::cout << "Opciones:\n";
-    std::cout << "  --help    Muestra este mensaje de ayuda y termina\n";
-    std::cout << "\nFormato del fichero de especificación del autómata finito:\n";
-    std::cout << "  - Línea 1: Símbolos del alfabeto separados por espacios.\n";
-    std::cout << "  - Línea 2: Número total de estados del autómata.\n";
-    std::cout << "  - Línea 3: Estado de arranque del autómata.\n";
-    std::cout << "  - A continuación, una línea para cada estado con:\n";
-    std::cout << "    * Número identificador del estado.\n";
-    std::cout << "    * Un 1 si es un estado de aceptación, 0 en caso contrario.\n";
-    std::cout << "    * Número de transiciones que posee el estado.\n";
-    std::cout << "    * Detalle de cada transición con:\n";
-    std::cout << "      - Símbolo de entrada necesario para la transición (use '&' para cadena vacía).\n";
-    std::cout << "      - Estado destino de la transición.\n";
-    std::cout << "\nEjemplo de fichero .fa:\n";
-    std::cout << "  a b\n";
-    std::cout << "  3\n";
-    std::cout << "  0\n";
-    std::cout << "  0 0 2 a 1 b 2\n";
-    std::cout << "  1 1 1 b 2\n";
-    std::cout << "  2 0 1 a 0\n";
-    return false;
-  } else {
+  if (argc == 1) {
     std::cout << "Modo de empleo: ./p06_automata_simulator input.fa input.txt" << std::endl;
     std::cout << "Pruebe ’p06_automata_simulator --help’ para más información." << std::endl;
     return false;
-  }
+  } else {
+    std::string help = argv[1];
+    if (argc == kLimit) {
+      return true;
+    } else if (argc == 2 && help == "--help") {
+      std::cout << "Uso: p06_automata_simulator [opciones]\n";
+      std::cout << "Opciones:\n";
+      std::cout << "  --help    Muestra este mensaje de ayuda y termina\n";
+      std::cout << "\nFormato del fichero de especificación del autómata finito:\n";
+      std::cout << "  - Línea 1: Símbolos del alfabeto separados por espacios.\n";
+      std::cout << "  - Línea 2: Número total de estados del autómata.\n";
+      std::cout << "  - Línea 3: Estado de arranque del autómata.\n";
+      std::cout << "  - A continuación, una línea para cada estado con:\n";
+      std::cout << "    * Número identificador del estado.\n";
+      std::cout << "    * Un 1 si es un estado de aceptación, 0 en caso contrario.\n";
+      std::cout << "    * Número de transiciones que posee el estado.\n";
+      std::cout << "    * Detalle de cada transición con:\n";
+      std::cout << "      - Símbolo de entrada necesario para la transición (use '&' para cadena vacía).\n";
+      std::cout << "      - Estado destino de la transición.\n";
+      std::cout << "\nEjemplo de fichero .fa:\n";
+      std::cout << "  a b\n";
+      std::cout << "  3\n";
+      std::cout << "  0\n";
+      std::cout << "  0 0 2 a 1 b 2\n";
+      std::cout << "  1 1 1 b 2\n";
+      std::cout << "  2 0 1 a 0\n";
+      return false;
+    }
 }
